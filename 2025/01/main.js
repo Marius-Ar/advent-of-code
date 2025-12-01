f9855d67765a8f6050e0a50a87550fc3e5acf1d6
marius-ar <mariusaillerie@gmail.com>
2025-12-01 22:31:26 +0100

    12/01/2025

---
+++ 2025/01/main.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const rawInput = fs.readFileSync(__dirname + '/input.txt').toString()
+let password = 0
+rawInput.split('\n')
+    .map(s => {
+        const firstChar = s.charAt(0)
+        const number = parseInt(s.substring(1)) % 100
+        return firstChar === 'L' ? -number : number
+    })
+    .reduce((a, b) => {
+        const stepSum = a + b
+        let finalStepSum = stepSum
+
+        if (stepSum > 99) {
+            finalStepSum -= 100
+        } else if (stepSum < 0) {
+            finalStepSum = 100 + stepSum
+        }
+
+        if (finalStepSum === 0) {
+            password++;
+        }
+        return finalStepSum
+    }, 50)
+
+console.log(password)
